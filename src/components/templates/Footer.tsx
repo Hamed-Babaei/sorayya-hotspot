8cdf61adad579fce5c22fd6d3acf963ad6dd4aa9
Hamed Babaei <dev.hamedbabaei@gmail.com>
2024-10-17 14:26:43 +0330

    develope footer (fix bug)

--- src/components/templates/Footer.tsx
+++ src/components/templates/Footer.tsx
@@ -6,21 +6,19 @@ import React from "react";
 export default function Footer() {
   const { theme } = useTheme();
   return (
-    <div className="flex items-center justify-center h-12">
+    <div className="flex items-center justify-center h-12 gap-2">
       {theme === "light" ? (
-        <img
-          src={"/images/s-logo-light.svg"}
-          alt="logo"
-          className="w-10 me-3 border-e border-black"
-        />
+        <>
+          <img src={"/images/s-logo-light.svg"} alt="logo" className="w-10" />
+          <span className="h-10 flex items-center">|</span>
+        </>
       ) : (
-        <img
-          src={"/images/s-logo-dark.png"}
-          alt="logo"
-          className="w-10 me-3 border-e border-white"
-        />
+        <>
+          <img src={"/images/s-logo-dark.png"} alt="logo" className="w-10" />
+          <span className="h-10 flex items-center">|</span>
+        </>
       )}
-      <p className="text-sm text-center text-gray-500 font-semibold dark:text-white/70">
+      <p className="text-sm text-center text-gray-500 font-semibold dark:text-white/80">
         تحقیق و توسعه توسط
         <span className="text-black font-bold ps-1 dark:text-white">
           شرکت فناور بنیان ثریا
